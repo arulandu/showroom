b6a47346bef7bc2444bde2438ca5efc4ae37fc7c
Claeb101 <calebarulandu@gmail.com>
2023-07-06 14:11:54 -0400

    trigger

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 import { Navigation } from '@/app/nav'
 import Link from 'next/link'
 
-
-
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
