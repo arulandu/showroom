71797b56b3d8ab96a3e7cfabfea5778a44559a19
Claeb101 <calebarulandu@gmail.com>
2023-07-12 04:58:19 -0400

    better heartbeat

--- app/src/app/api/route.ts
+++ app/src/app/api/route.ts
@@ -6,8 +6,9 @@ export async function GET(req: NextRequest) {
   const session = await getServerSession()
   const token = await getToken({ req })
   const gh = await (await fetch("https://api.github.com/repos/Claeb101/showroom/commits/main")).json()
-  const status = `✅ Last commit: ${gh.commit.tree.sha} by ${gh.commit.committer.name} on ${gh.commit.committer.date}`
-  const res = `${status}\n\nSession: ${JSON.stringify(session, null, 4)}\n\nToken: ${JSON.stringify(token, null, 4)}`
+  console.log(gh.commit)
+  const status = `✅ Last commit: ${gh.commit.tree.sha} "${gh.commit.message}" by ${gh.commit.committer.name} on ${gh.commit.committer.date}`
+  const res = `Today: ${(new Date()).toISOString()}\n\n${status}\n\nSession: ${JSON.stringify(session, null, 4)}\n\nToken: ${JSON.stringify(token, null, 4)}`
   return new NextResponse(res)
 
 }
