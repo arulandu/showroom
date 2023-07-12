54be60a19fd023b82d42125506bd78d20433e438
Claeb101 <calebarulandu@gmail.com>
2023-07-12 04:58:36 -0400

    no log

--- app/src/app/api/route.ts
+++ app/src/app/api/route.ts
@@ -6,7 +6,6 @@ export async function GET(req: NextRequest) {
   const session = await getServerSession()
   const token = await getToken({ req })
   const gh = await (await fetch("https://api.github.com/repos/Claeb101/showroom/commits/main")).json()
-  console.log(gh.commit)
   const status = `✅ Last commit: ${gh.commit.tree.sha} "${gh.commit.message}" by ${gh.commit.committer.name} on ${gh.commit.committer.date}`
   const res = `Today: ${(new Date()).toISOString()}\n\n${status}\n\nSession: ${JSON.stringify(session, null, 4)}\n\nToken: ${JSON.stringify(token, null, 4)}`
   return new NextResponse(res)
