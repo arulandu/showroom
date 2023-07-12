bef61484ee51fa83e1a23e1d063567de599556c9
Claeb101 <calebarulandu@gmail.com>
2023-07-12 04:59:53 -0400

    center text

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <>
-      <div className=' max-h-screen flex-grow flex flex-col items-center justify-center'>
+      <div className=' max-h-screen flex-grow flex flex-col items-center justify-center text-center'>
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Joven Motors
         </h1>
