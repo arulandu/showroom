4c125313781138b667e3ee8b47d70c14394b2f85
Claeb101 <calebarulandu@gmail.com>
2023-07-19 18:23:14 -0400

    better receipt

--- app/src/app/dashboard/order/[id]/receipt.tsx
+++ app/src/app/dashboard/order/[id]/receipt.tsx
@@ -1,60 +1,77 @@
 "use client"
 import { Button } from "@/components/ui/button";
 import { OrderItem } from "@prisma/client";
-import { Document, PDFDownloadLink, PDFViewer, Page, Text, View } from "@react-pdf/renderer";
+import { Document, Font, PDFDownloadLink, PDFViewer, Page, Text, View } from "@react-pdf/renderer";
 
 import { StyleSheet } from '@react-pdf/renderer'
 
-// // Register font
-// Font.register({ family: 'Roboto', src: source });
-
-// Reference font
-const styles = StyleSheet.create({
-  document: {
-    fontFamily: 'Courier'
-  }
-})
+Font.register({
+  family: 'Custom',
+  fonts: [
+    {
+      src: 'http://fonts.gstatic.com/s/inconsolata/v31/QldgNThLqRwH-OJ1UHjlKENVzkWGVkL3GZQmAwLYxYWI2qfdm7Lpp4U8aRr8lleY2co.ttf',
+    },
+    {
+      src: "http://fonts.gstatic.com/s/inconsolata/v31/QldgNThLqRwH-OJ1UHjlKENVzkWGVkL3GZQmAwLYxYWI2qfdm7Lpp2I7aRr8lleY2co.ttf",
+      fontWeight: "bold"
+    }
+  ],
+});
 
 export const Receipt = ({ order }: { order: any }) => {
   const Doc = () => (
-    <Document title={`order_${order.id}`} style={styles.document}>
-      <Page size="A4" style={{ margin: 10 }}>
-        <View>
-          <Text style={{ textDecoration: "underline" }}>Metadata</Text>
-          <Text>Date: {order.createdAt.toLocaleString()}</Text>
-          <Text>Order Id: {order.id}</Text>
-          <Text>Showroom Agent: {order.employee.name}</Text>
-          <Text>Customer: {order.customer.name} | {order.customer.email}</Text>
+    <Document title={`order_${order.id}`} style={{fontFamily: "Custom"}}>
+      <Page size="A4">
+        <View style={{ padding: 20 }}>
+          <View>
+            <View style={{ textAlign: "center" }}>
+              <Text style={{ fontWeight: "bold"}}>Joven Motors</Text>
+              <Text>VJ37+8V4, road, Madurai Main, Kalayarkoil, Tamil Nadu 630551, India</Text>
+            </View>
+
+            <View style={{ textAlign: "center", marginVertical: 10 }}>
+              <Text>{order.createdAt.toLocaleString()}</Text>
+              <Text>Showroom Agent: {order.employee.name}</Text>
+              <Text>Customer: {order.customer.name} | {order.customer.email}</Text>
+              <Text >Order Id: {order.id}</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={{ textDecoration: 'underline', marginTop: 10 }}>Items</Text>
+            {order.items.map((item: any) =>
+              <Text key={item.id}>{item.quantity}x --- {item.product.name} --- ₹{item.price.toFixed(2)} e.a. --- (C {item.product.cgstTaxRate * 100}% S {item.product.sgstTaxRate * 100}%)</Text>
+            )}
+          </View>
+          <View style={{ marginTop: 30, textAlign: "right" }}>
+            <Text>Total: ₹{order.invoice.amount.toFixed(2)}</Text>
+            <View>
+              {
+                order.invoice.payments.map((payment: any) => <Text key={payment.id}>* {payment.method} ₹{payment.amount.toFixed(2)}</Text>)
+              }
+            </View>
+            {order.amountOwed > 0 ?
+              <Text style={{ fontWeight: "bold" }}>Outstanding Amount: ₹{order.amountOwed.toFixed(2)}</Text>
+              : null}
+          </View>
         </View>
-        <View>
-          <Text style={{ textDecoration: 'underline', marginTop: 10 }}>Items</Text>
-          {order.items.map((item: any) =>
-            <Text key={item.id}>{item.quantity}x | {item.price.toFixed(2)} r.s. (cgst: {item.product.cgstTaxRate * 100}%, sgst: {item.product.sgstTaxRate * 100}%)</Text>
-          )}
-        </View>
-        <View>
-          <Text style={{ textDecoration: 'underline', marginTop: 10 }}>Summary</Text>
-          <Text>Total Amount Owed: {order.invoice.amount.toFixed(2)}</Text>
-          <Text>Total Amount Paid: {order.invoice.amountPaid.toFixed(2)}</Text>
-          {order.amountOwed > 0 ?
-            <Text style={{ color: "red" }}>Outstanding Amount: {order.amountOwed.toFixed(2)}</Text>
-            : null}
-        </View>
+
       </Page>
     </Document>
   )
 
   return (
     <div className="w-full flex-grow flex flex-col">
-      <PDFDownloadLink document={<Doc />} fileName={`order_${order.id}.pdf`}>
-        {({ blob, url, loading, error }) =>
-          <div className="w-full flex items-center justify-center">
-            <Button disabled={loading} className="my-4">
-              Download Reciept {"->"}
-            </Button>
-          </div>
-        }
-      </PDFDownloadLink>
+      <div className="w-full flex items-center justify-center my-4">
+        <PDFDownloadLink document={<Doc />} fileName={`order_${order.id}.pdf`}>
+          {({ blob, url, loading, error }) =>
+            <div className="">
+              <Button disabled={loading}>
+                Download Reciept {"->"}
+              </Button>
+            </div>
+          }
+        </PDFDownloadLink>
+      </div>
       <PDFViewer className="w-full flex-grow" >
         <Doc />
       </PDFViewer>
