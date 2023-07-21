1b1d8833611e055bfa1d7fea8385a9644116a306
Claeb101 <calebarulandu@gmail.com>
2023-07-21 15:47:45 -0400

    migrated to #13

--- app/src/app/dashboard/customer/search.tsx
+++ app/src/app/dashboard/customer/search.tsx
@@ -13,7 +13,7 @@ import { useState } from "react"
 export const CustomerSearch = () => {
   const { toast } = useToast()
   const router = useRouter()
-  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
   const [processing, setProcessing] = useState(false)
 
   const search = async () => {
@@ -21,16 +21,24 @@ export const CustomerSearch = () => {
     try {
       setProcessing(true)
       
-      const customer = (await (await fetch("/api/customer?" + new URLSearchParams({email}))).json()).customer
-      if (!customer) throw Error()
+      const {customers} = (await (await fetch("/api/customer?" + new URLSearchParams({phone}))).json())
+      if (!customers || customers.length == 0) throw Error()
 
-      setProcessing(false)
+      if(customers.length > 1) {
+        // TODO: step through the customers with arrows?
+        toast({title: "Error", description: "Multiple customers with this search have been found. Please specify."})
+        return
+      }
+
+      const customer = customers[0]
 
       toast({ title: "Found customer." })
       router.push(`/dashboard/customer/${customer.id}`)
     } catch {
       toast({ title: "Not found.", description: "This customer's details was not found in our system. please update and try again. If this is the customer's first recorded entry, create a new customer." })
     }
+
+    setProcessing(false)
   }
 
   return (
@@ -43,8 +51,8 @@ export const CustomerSearch = () => {
         <form>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="employee@gmail.com" value={email} onChange={handleChange(setEmail)} />
+              <Label htmlFor="phone">Phone</Label>
+              <Input id="phone" placeholder="+1-(123)-456-7890" value={phone} onChange={handleChange(setPhone)} />
             </div>
             <Button variant="secondary" disabled={processing ? true : false} onClick={(e) => {e.preventDefault(); search()}} className="flex-grow">Search</Button>
           </div>
